f35f028458d7d6d576139bdd824ec1294db76b97
Javi Calonge <javicalongem@gmail.com>
2024-10-07 11:36:27 +0200

    Actualizar JS portfolio

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,4 +1,4 @@
-//API
+// API
 const apiKey = "b81bsES8bbg7TZiVMqZa9Vfbo3eonwBZtSi2ehQpzxzn2bvlUgFfBbJl";
 const searchResult = document.getElementById("result");
 
@@ -9,24 +9,39 @@ const initialImageLoad = 9;
 
 const searchQueries = ["nature", "bar", "cats", "animals", "cities", "birds"];
 
-//EVENT LISTENERS
+// EVENT LISTENERS
 const setupListeners = () => {
-  document.addEventListener("DOMContentLoaded", onPageLoad);
+  document.addEventListener("DOMContentLoaded", setupImageObserver);
 };
 
-//EVENT HANDLERS
-const onPageLoad = () => {
+// EVENT HANDLERS
+const setupImageObserver = () => {
+  const imageContainer = document.querySelector(".container-img");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        observer.disconnect();
+        loadInitialImages();
+      }
+    });
+  });
+
+  observer.observe(imageContainer);
+};
+
+// Cargar imágenes iniciales cuando el contenedor entra en vista
+const loadInitialImages = () => {
   const query = getRandomQuery();
   const apiURL = `https://api.pexels.com/v1/search?query=${query}&orientation=landscape&per_page=${initialImageLoad}`;
 
-  showLoading();
-
   fetchImages(apiURL)
     .then((data) => displayResults(data))
-    .finally(hideLoading);
+    .catch((error) =>
+      console.error("Error al cargar imágenes iniciales:", error)
+    );
 };
 
-// Función para obtener una búsqueda aleatoria
 const getRandomQuery = () => {
   const randomIndex = Math.floor(Math.random() * searchQueries.length);
   return searchQueries[randomIndex];
@@ -37,7 +52,6 @@ const displayResults = (data) => {
   removeObserver();
 
   data.photos.forEach((photo) => {
-    // Solo cargar si el total de imágenes es menor al límite
     if (totalImagesLoaded < maxImages) {
       searchResult.innerHTML += `
       <div class="grid-item">
@@ -56,6 +70,7 @@ const displayResults = (data) => {
   createObserver(data.next_page);
 };
 
+// Spinner functions
 const showLoading = () => {
   const div = document.createElement("div");
   div.classList.add("loader");
@@ -69,17 +84,18 @@ const hideLoading = () => {
   if (loader) {
     setTimeout(() => {
       loader.remove();
-    }, 500);
+    }, 300);
   }
 };
 
+// Lazy Loading Functions
 const createObserver = (nextPageURL) => {
   if (!nextPageURL || totalImagesLoaded >= maxImages) return;
 
   const sentinel = document.createElement("div");
   sentinel.id = "sentinel";
 
-  document.querySelector(".container").appendChild(sentinel);
+  document.querySelector(".container-img").appendChild(sentinel);
 
   sentinelObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
