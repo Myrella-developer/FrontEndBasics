3e1d9f6f0187bf4759874009bd38d3a4104b88da
08000-OCU-TANDEM Juan Jose Arcila Duque <juanjose.arcila@creuroja.org>
2024-10-07 16:01:23 +0200

    prueba javascript

---
+++ Javascript/funtion.js
@@ -0,0 +1,6 @@
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('exampleInputEmail');
+const phoneInput = document.getElementById('phone');
+const subjectInput = document.getElementById('subject');
+const messageInput = document.getElementById('message');
+const submitButton = document.getElementById('submit');
