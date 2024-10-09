d4fab5d63ed483489f1397a2e0bf77938b4434b4
collajuan <collajuan@gmail.com>
2024-10-09 19:51:44 +0200

    traslate navbar

--- script/traslate.js
+++ script/traslate.js
@@ -3,8 +3,8 @@ buttonsLang = document.querySelectorAll(".btn-lang"),
 //listener a todos los botones del tipo btn-lang
 buttonsLang.forEach(element => {
     element.addEventListener('click', () => {
-        setLanguage(element.id)
         //!Aqui ejecutar la funcion de cambiar idioma
+        setLanguage(element.id)
     })
 });
 
@@ -12,11 +12,34 @@ buttonsLang.forEach(element => {
 function setLanguage(lang){
     //titulo Ppal
     document.querySelector('h1').innerText = tituloPpal[lang]
-    //titulos de seccion
+    //navbar
+    linksNavbar = document.querySelectorAll('.navbar-nav a')
+    console.log(linksNavbar);
+    linksNavbar.forEach((element) => {
+        element.textContent = navbar[lang][element.id]
+    })
+    //titulos de seccián
     titulosSeccion = document.querySelectorAll('h2')
     titulosSeccion.forEach((element, index) => {
-        element.textContent = titulos[lang][`titulo${index+1}`]
+        element.textContent = titulos[lang][`titulo${(index+1)*10}`]
     })
+}
+
+let navbar = {
+    es: {
+        navbara10: 'Quiénes somos?',
+        navbara20: 'Servicios',
+        navbara30: 'Portafolio',
+        navbara40: 'Ubicación',
+        navbara50: 'Contáctanos'
+    },
+    en: {
+        navbara10: 'Who we are?',
+        navbara20: 'Services',
+        navbara30: 'Portfolio',
+        navbara40: 'Location',
+        navbara50: 'Email Us'
+    }
 }
 
 let tituloPpal = {
@@ -26,17 +49,17 @@ let tituloPpal = {
 
 let titulos = {
     es : {
-        titulo1 : 'Quiénes somos?',
-        titulo2 : 'Nuestros servicios',
-        titulo3 : 'Portafolio',
-        titulo4 : 'Ubicación',
-        titulo5 : 'Contáctanos'
+        titulo10: 'Quiénes somos?',
+        titulo20: 'Nuestros servicios',
+        titulo30: 'Portafolio',
+        titulo40: 'Ubicación',
+        titulo50: 'Contáctanos'
     },
     en : {
-        titulo1 : 'Who we are?',
-        titulo2 : 'Our Services',
-        titulo3 : 'Portfolio',
-        titulo4 : 'Location',
-        titulo5 : 'Email US'
+        titulo10: 'Who we are?',
+        titulo20: 'Our Services',
+        titulo30: 'Portfolio',
+        titulo40: 'Location',
+        titulo50: 'Email US'
     }
 }
