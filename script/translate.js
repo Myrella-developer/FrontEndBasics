f8b7668653c2e6ce73e37168c8c0502c76ec5650
collajuan <collajuan@gmail.com>
2024-10-18 20:07:58 +0200

    se optimiza translate

--- script/translate.js
+++ script/translate.js
@@ -1,24 +1,26 @@
 const buttonsLang = document.querySelectorAll(".btn-lang")
 
 //Se carga idioma por defecto
-if (localStorage.getItem("lang")) {
-    if (localStorage.getItem("lang") == "es") setLanguage("es")
-    else if (localStorage.getItem("lang") == "en") setLanguage("en")
-} else localStorage.setItem("lang", "es")
+const localLang = localStorage.getItem("lang")
+if (localLang == 'es' || localLang == 'en') setLanguage(localLang)
+else {
+    localStorage.setItem("lang", 'es')   
+    setLanguage('es')
+} 
 
 //listener a todos los botones del tipo btn-lang
 buttonsLang.forEach(element => {
     element.addEventListener('click', () => {
-        //!Aqui ejecutar la funcion de cambiar idioma
         setLanguage(element.id)
+        localStorage.setItem("lang", element.id)   
     })
 });
 
 
 function setLanguage(lang){
-    
-    for (const texto in traducciones[lang]) {      
-        document.querySelector(`#${texto}`).textContent = contenidoSecciones[lang][texto]
+//Busca todos los id en el Objeto traducciones y lo modifica uno a uno en el dom segun su id    
+    for (const tagId in traducciones[lang]) {      
+        document.querySelector(`#${tagId}`).textContent = traducciones[lang][tagId]
     }
 }
 
